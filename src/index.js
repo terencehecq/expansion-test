1e6a3eaf91362f3471630336123d842bae90b569
Térence <terencehecq@gmail.com>
2020-10-19 13:04:42 +0200

    finish css + animations

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
 import _ from 'lodash';
 
 
-// -- Cursor -- //
+// -- CURSOR -- //
 let cursor = document.getElementById('cursor');
+let search = document.getElementById("search_icon");
+let field = document.getElementById("search_field");
 
 document.addEventListener('mousemove', (e)=>{
-  let x = e.pageX; 
-  let y = e.pageY; 
 
-  cursor.style.left = x + 'px';
-  cursor.style.top = (y -47) + 'px';
+  // Cursor positionning
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.left = (x-5) + 'px';
+  cursor.style.top = (y-55) + 'px';
+
+  // hover on search
+  let searchPos = search.getBoundingClientRect();
+  let dFromCenter = searchPos.width/1.2;
+  let searchX = searchPos.left + searchPos.width/2;
+  let searchY = searchPos.top + searchPos.width/2;
+  let cx = e.clientX;
+  let cy = e.clientY;
+
+  console.log(cx-searchX," - ", cy-searchY);
+
+  if(cx > searchX-dFromCenter && cx < searchX+dFromCenter && cy > searchY-dFromCenter && cy < searchY+dFromCenter){
+    search.style.transform = "translate("+ Math.round(cx-searchX) + "px," + Math.round(cy-searchY) +"px)";
+    cursor.classList.add("hover");
+    search.classList.add("hover")
+  }else{
+    search.style.transform = "translate(0,0)";
+    cursor.classList.remove("hover");
+    search.classList.remove("hover");
+  }
 });
+
+
+
+// -- Focus on search field -- //
+search.addEventListener("click", ()=>{
+  field.focus();
+});
+
 
 
 
@@ -40,6 +72,7 @@ mainLinks.forEach((link)=>{
     }
   })
 })
+
 
 // -- Toggle drop "Plus" menu -- //
 let more = document.getElementById("more");
