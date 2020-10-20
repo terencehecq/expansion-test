f3e3b8206440e6ac435a2279c074143f61bd3e4c
Térence <terencehecq@gmail.com>
2020-10-20 19:59:00 +0200

    cursor issue

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ document.addEventListener('mousemove', (e)=>{
   let x = e.pageX;
   let y = e.pageY;
 
-  cursor.style.left = x + 'px';
-  cursor.style.top = y + 'px';
+  cursor.style.left = (x) + 'px';
+  cursor.style.top = (y-50) + 'px';
 
   // hover on search
   let searchPos = search.getBoundingClientRect();
